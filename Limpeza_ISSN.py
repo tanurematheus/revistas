28f29ee40609e36bc47c103e952c90f748d18d56
Matheus Tanure <tanure.matheus@gmail.com>
2021-04-04 20:04:10 -0300

    Limpeza_ISSN

---
+++ Limpeza_ISSN.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+teste = pd.read_excel('teste.xlsx')
+
+df = pd.DataFrame(teste)
+
+lista_ISSN = list()
+
+for x in df.index:
+    ISSN = df.loc[x,"ISSN"]
+    print(x)  
+    if ISSN not in lista_ISSN:
+        lista_ISSN.append(ISSN)        
+    else:
+        df.drop(x, inplace = True)
+    
+df.to_excel (r'ISSN.xlsx',index = False)
